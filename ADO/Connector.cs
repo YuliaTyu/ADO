d167fd8a9ff6c2f8b0946bbd3c0f70767c594581
Юля <@DESKTOP-LDN2BMM>
2026-03-01 21:34:57 +0300

    DLL in ADO

--- ADO/Connector.cs
+++ ADO/Connector.cs
@@ -10,91 +10,92 @@ namespace ADO
 {
     internal class Connector
     {
-        string connection_string;
-        SqlConnection connection;
-        public Connector(string connection_string)
-        {
-            this.connection_string = connection_string;
-            this.connection = new SqlConnection(connection_string);
-        }
-        public void Select(string cmd)
-        {
-            //открываем соединение
-            connection.Open();
-            SqlCommand command = new SqlCommand(cmd, connection);
 
-            //прочитать данные из базы - открываем reader
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");//если занем скольк полей
-                for (int i = 0; i < reader.FieldCount; i++)//если не занем сколько полей
-                {
-                    Console.Write(reader[i].ToString().PadRight(29));
-                }
-                Console.WriteLine();
-            }
-            //зарываем reader
-            reader.Close();
+        //string connection_string;
+        //SqlConnection connection;
+        //public Connector(string connection_string)
+        //{
+        //    this.connection_string = connection_string;
+        //    this.connection = new SqlConnection(connection_string);
+        //}
+        //public void Select(string cmd)
+        //{
+        //    //открываем соединение
+        //    connection.Open();
+        //    SqlCommand command = new SqlCommand(cmd, connection);
 
-            //закрваем соединение
-            connection.Close();
-        }
-        public void Select(string fields, string tables, string condition = "")
-        {
-            string cmd = $"SELECT {fields} FROM {tables}";
-            if (condition != "") cmd += $" WHERE {condition}";
-            cmd += ";";
-            Select(cmd);
+        //    //прочитать данные из базы - открываем reader
+        //    SqlDataReader reader = command.ExecuteReader();
+        //    while (reader.Read())
+        //    {
+        //        //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");//если занем скольк полей
+        //        for (int i = 0; i < reader.FieldCount; i++)//если не занем сколько полей
+        //        {
+        //            Console.Write(reader[i].ToString().PadRight(29));
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    //зарываем reader
+        //    reader.Close();
 
-        }
+        //    //закрваем соединение
+        //    connection.Close();
+        //}
+        //public void Select(string fields, string tables, string condition = "")
+        //{
+        //    string cmd = $"SELECT {fields} FROM {tables}";
+        //    if (condition != "") cmd += $" WHERE {condition}";
+        //    cmd += ";";
+        //    Select(cmd);
+
+        //}
         
-        //вставить поле с режиссером - ЗАДВОЕНИЕ ЗАПИСИ!!!! ПРЕДОТВРАТИТЬ НЕОБХОДИМО в программ не иделален код
-        public void Insert(string cmd)
-        {
-            connection.Open();
-            SqlCommand command = new SqlCommand(cmd, connection);
-            command.ExecuteNonQuery();
-            connection.Close();
-        }
-        public void Insert(string table, string values)
-        {
-            string cmd =            
-                $"INSERT INTO {table} VALUES ({values})";
-            Insert(cmd);
-        }
-        public object Scalar(string cmd)//скалярный запрос - вытаскивающий из базы одно значение
-        {
-            SqlCommand command = new SqlCommand(cmd, connection);
-            connection.Open();
-            //int value = Convert.ToInt32(command.ExecuteScalar());
-            object value = command.ExecuteScalar();
-            connection.Close();
-            return value;
-        }
-        public string GetPrimaryKeyColumn(string table)
-        {
-            return (string)Scalar
-                (
-                $"SELECT\tCOLUMN_NAME FROM\tINFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE WHERE\tCONSTRAINT_NAME=(SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME=N'{table}' AND CONSTRAINT_TYPE=N'PRIMARY KEY');"
-                );
-        }
-        public int GetLastPrimaryKey(string table)
-        {
-            return Convert.ToInt32(Scalar($"SELECT MAX({GetPrimaryKeyColumn(table)}) FROM {table}"));
-        }
-        public int GetNextPrimaryKey(string table)//узнаем имя первичного ключа
-        {
-            return GetLastPrimaryKey(table) + 1;
-        }
+        ////вставить поле с режиссером - ЗАДВОЕНИЕ ЗАПИСИ!!!! ПРЕДОТВРАТИТЬ НЕОБХОДИМО в программ не иделален код
+        //public void Insert(string cmd)
+        //{
+        //    connection.Open();
+        //    SqlCommand command = new SqlCommand(cmd, connection);
+        //    command.ExecuteNonQuery();
+        //    connection.Close();
+        //}
+        //public void Insert(string table, string values)
+        //{
+        //    string cmd =            
+        //        $"INSERT INTO {table} VALUES ({values})";
+        //    Insert(cmd);
+        //}
+        //public object Scalar(string cmd)//скалярный запрос - вытаскивающий из базы одно значение
+        //{
+        //    SqlCommand command = new SqlCommand(cmd, connection);
+        //    connection.Open();
+        //    //int value = Convert.ToInt32(command.ExecuteScalar());
+        //    object value = command.ExecuteScalar();
+        //    connection.Close();
+        //    return value;
+        //}
+        //public string GetPrimaryKeyColumn(string table)
+        //{
+        //    return (string)Scalar
+        //        (
+        //        $"SELECT\tCOLUMN_NAME FROM\tINFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE WHERE\tCONSTRAINT_NAME=(SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME=N'{table}' AND CONSTRAINT_TYPE=N'PRIMARY KEY');"
+        //        );
+        //}
+        //public int GetLastPrimaryKey(string table)
+        //{
+        //    return Convert.ToInt32(Scalar($"SELECT MAX({GetPrimaryKeyColumn(table)}) FROM {table}"));
+        //}
+        //public int GetNextPrimaryKey(string table)//узнаем имя первичного ключа
+        //{
+        //    return GetLastPrimaryKey(table) + 1;
+        //}
 
-        public void Update(string cmd)
-        {
-            SqlCommand command = new SqlCommand(cmd, connection);
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
-        }
+        //public void Update(string cmd)
+        //{
+        //    SqlCommand command = new SqlCommand(cmd, connection);
+        //    connection.Open();
+        //    command.ExecuteNonQuery();
+        //    connection.Close();
+        //}
 
     }
 }
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -11,29 +11,30 @@ namespace ADO
     {
         static void Main(string[] args)
         {
+            
             //создаем соедиенеие
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_SPU_411;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";//из свойств Movies_SPU_411 вытаскавем строку 
             SqlConnection connection = new SqlConnection(connection_string);
             string cmd = "SELECT title,year,first_name,last_name FROM Movies JOIN Directors ON(director=director_id)";
             
-            Connector connector = new Connector(connection_string);
-            connector.Select("title,year,first_name,last_name","Movies,Directors","director=director_id");
-            Console.WriteLine("\n------------------ -\n");
+            //Connector connector = new Connector(connection_string);
+            //connector.Select("title,year,first_name,last_name","Movies,Directors","director=director_id");
+            //Console.WriteLine("\n------------------ -\n");
 
-            //Console.WriteLine(connector.Scalar("SELECT MAX(director_id)FROM Directors"));
+            ////Console.WriteLine(connector.Scalar("SELECT MAX(director_id)FROM Directors"));
 
-            string table = "Movies";
-            Console.WriteLine(connector.GetNextPrimaryKey(table));
-            Console.WriteLine(connector.GetLastPrimaryKey(table));
-            Console.WriteLine(connector.GetPrimaryKeyColumn(table));
+            //string table = "Movies";
+            //Console.WriteLine(connector.GetNextPrimaryKey(table));
+            //Console.WriteLine(connector.GetLastPrimaryKey(table));
+            //Console.WriteLine(connector.GetPrimaryKeyColumn(table));
 
-            connector.Insert($"IF NOT EXISTS (SELECT * FROM Directors WHERE last_name=N'Besson' AND first_name=N'Luc') " +
-                $"INSERT Directors VALUES({connector.GetNextPrimaryKey("Directors")},N'Besson',N'Luc')");//запись не дублируется!!! но код не идеален
-            //connector.Insert("Directors", $"{connector.GetNextPrimaryKey("Directors")},N'Besson',N'Luc'");//добавляем режиссера!!!!!!!!!!!
-            connector.Update("UPDATE Directors SET last_name=N'Lettich',first_name=N'Sheldon' WHERE director_id=8");
-            connector.Select("*", "Directors");
-            Console.WriteLine("\n------------------ -\n");
-            connector.Select(cmd);
+            //connector.Insert($"IF NOT EXISTS (SELECT * FROM Directors WHERE last_name=N'Besson' AND first_name=N'Luc') " +
+            //    $"INSERT Directors VALUES({connector.GetNextPrimaryKey("Directors")},N'Besson',N'Luc')");//запись не дублируется!!! но код не идеален
+            ////connector.Insert("Directors", $"{connector.GetNextPrimaryKey("Directors")},N'Besson',N'Luc'");//добавляем режиссера!!!!!!!!!!!
+            //connector.Update("UPDATE Directors SET last_name=N'Lettich',first_name=N'Sheldon' WHERE director_id=8");
+            //connector.Select("*", "Directors");
+            //Console.WriteLine("\n------------------ -\n");
+            //connector.Select(cmd);
 
 
         }
